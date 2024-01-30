a36a5eb0b6e09d3dda143c8d403e1d6f55f1c0c0
IliaKachnov <123602127+IliaKachnov@users.noreply.github.com>
2024-01-30 13:25:06 +0100

    Add files via upload

---
+++ scripts/aquaForm.js
@@ -0,0 +1,78 @@
+const counterMinus = document.querySelector('.counter__minus');
+const counterAmount = document.getElementById('counterAmount');
+
+function decrement() {
+  let currentValue = parseInt(counterAmount.textContent);
+
+  if (currentValue > 1) {
+    currentValue--;
+    counterAmount.textContent = currentValue;
+
+    updateClass();
+  }
+}
+
+function increment() {
+  let currentValue = parseInt(counterAmount.textContent);
+  currentValue++;
+  counterAmount.textContent = currentValue;
+
+  updateClass();
+}
+
+function updateClass() {
+  const isActive = parseInt(counterAmount.textContent) > 1;
+  counterMinus.classList.toggle('active', isActive);
+}
+
+const tabs = document.querySelectorAll('.aquaForm-tab__btn');
+const all_content = document.querySelectorAll('.aquaForm__content ');
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener('click', (e) => {
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active');
+
+    let line = document.querySelector('.aquaForm__line');
+    line.style.width = e.target.offsetWidth + "px";
+    line.style.left = e.target.offsetLeft + "px"
+
+    all_content.forEach(content => {content.classList.remove('active')})
+    all_content[index].classList.add('active');
+  })
+  
+})
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const input = document.querySelector('.custom__date');
+  const label = document.querySelector('.aquaForm__input');
+
+  input.addEventListener('focus', function() {
+      label.classList.add('active');
+  });
+
+  input.addEventListener('blur', function() {
+      if (!input.value) {
+          label.classList.remove('active');
+      }
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const inputs = document.querySelectorAll('.aquaForm__input-inner');
+
+  inputs.forEach(function(input) {
+      input.addEventListener('focus', function() {
+          input.closest('.aquaForm__input').classList.add('active');
+      });
+
+      input.addEventListener('blur', function() {
+          if (!input.value) {
+              input.closest('.aquaForm__input').classList.remove('active');
+          }
+      });
+  });
+});
---
+++ scripts/aquaResult.js
@@ -0,0 +1,88 @@
+const openNameModal = document.getElementById('open__details');
+const closeNameModal = document.getElementById('popup__details-cross');
+const closeNameModalArrow = document.getElementById('popup__details-arrow');
+const modalName = document.querySelector('.popup__details');
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const inputElement = document.querySelector('.aquaResult__call-input.tel')
+  const maskOptions = { 
+    mask: '+{7}(000)000-00-00'
+  }
+  IMask(inputElement, maskOptions) 
+})
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const wrappers = document.querySelectorAll('.aquaResult__call-input-wrapper');
+
+  function checkInput(wrapper) {
+      const input = wrapper.querySelector('.aquaResult__call-input');
+      const required = wrapper.querySelector('.aquaResult__call-required');
+      
+      if (input.value.trim() !== '') {
+          required.style.display = 'none';
+      } else {
+          required.style.display = 'block';
+      }
+  }
+
+  wrappers.forEach(function(wrapper) {
+      const input = wrapper.querySelector('.aquaResult__call-input');
+
+      input.addEventListener('focus', function() {
+          wrapper.querySelector('.aquaResult__call-required').style.display = 'none';
+      });
+
+      input.addEventListener('input', function() {
+          checkInput(wrapper);
+      });
+
+      input.addEventListener('blur', function() {
+          checkInput(wrapper);
+      });
+  });
+});
+
+const swiper = new Swiper(".mySwiperAqua", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const aquaHeader = document.querySelector('.popup__details-header');
+  const aquaBody = document.querySelector('.popup__details');
+
+  aquaBody.addEventListener('scroll', function() {
+      if (aquaBody.scrollTop > 0) {
+        aquaHeader.classList.add('active');
+      } else {
+        aquaHeader.classList.remove('active');
+      }
+  });
+});
+
+openNameModal.addEventListener('click', () => {
+  modalName.classList.add('open');
+  document.body.style.overflow = 'hidden';
+});
+
+closeNameModal.addEventListener('click', () => {
+  modalName.classList.remove('open')
+  document.body.style.overflow = 'visible';
+});
+
+closeNameModalArrow.addEventListener('click', () => {
+  modalName.classList.remove('open')
+  document.body.style.overflow = 'visible';
+});
+
